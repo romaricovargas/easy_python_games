416cadaae594b6102d98a9136bbf843b2efd2d1d
Romarico Vargas <romaricovargas@yahoo.com>
2024-11-24 18:27:40 +1100

    changed height of display screen in practice 3

--- practice_files/03_blitting_images.py
+++ practice_files/03_blitting_images.py
@@ -6,7 +6,7 @@ pygame.init()
 
 #Create a display surface
 WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 900
+WINDOW_HEIGHT = 600
 DEFAULT_IMAGE_SIZE = (150, 150)
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Blitting Images!")
