047b2acfe8f9e589b8aab6512e5dbe98952d6259
Romarico Vargas <romaricovargas@yahoo.com>
2025-02-05 21:09:05 +1100

    added more codes to the game

--- you_are_what_you_eat/you_are_what_you_eat.py
+++ you_are_what_you_eat/you_are_what_you_eat.py
@@ -17,7 +17,7 @@ clock = pygame.time.Clock()
 
 #Set game values
 VELOCITY = 7
-points = 5 
+points = 0
 
 #Set colors
 CYAN = (0, 255, 255)
@@ -57,8 +57,17 @@ game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 #Set sounds and music
 food_sound_loc = os.path.join('you_are_what_you_eat', 'sounds', 'food.wav')
 food_sound = pygame.mixer.Sound(food_sound_loc)
+food_sound.set_volume(0.3)
 poison_sound_loc = os.path.join('you_are_what_you_eat', 'sounds', 'poison.wav')
 poison_sound = pygame.mixer.Sound(poison_sound_loc)
+poison_sound.set_volume(0.1)
+turn_good_sound_loc = os.path.join('you_are_what_you_eat', 'sounds', 'turn_good.wav')
+turn_good_sound = pygame.mixer.Sound(turn_good_sound_loc)
+turn_good_sound.set_volume(0.4)
+turn_evil_sound_loc = os.path.join('you_are_what_you_eat', 'sounds', 'turn_evil.wav')
+turn_evil_sound = pygame.mixer.Sound(turn_evil_sound_loc)
+turn_evil_sound.set_volume(0.2)
+
 music_loc = os.path.join('you_are_what_you_eat', 'sounds', 'kid-games-music-comedy-situation.mp3')
 pygame.mixer.music.load(music_loc)
 
@@ -181,9 +190,11 @@ while running:
         poison_01_image = pygame.transform.scale(poison_01_image, (80, 80))
         poison_01_rect = poison_01_image.get_rect()
         poison_01_rect.center = (random.randint(100, WINDOW_WIDTH-100), 110)
-        poison_01_speed = random.randint(2, 8)
+        poison_01_speed_y = random.randint(5, 9)
+        poison_01_speed_x = random.choice([-1, 1])
         poison_01_rendered = True
-    poison_01_rect.y += poison_01_speed
+    poison_01_rect.y += poison_01_speed_y
+    poison_01_rect.x += poison_01_speed_x
     display_surface.blit(poison_01_image, poison_01_rect)
 
     if not poison_02_rendered:
@@ -191,9 +202,11 @@ while running:
         poison_02_image = pygame.transform.scale(poison_02_image, (80, 80))
         poison_02_rect = poison_02_image.get_rect()
         poison_02_rect.center = (random.randint(100, WINDOW_WIDTH-100), 110)
-        poison_02_speed = random.randint(2, 8)
+        poison_02_speed_y = random.randint(5, 9)
+        poison_02_speed_x = random.choice([-1, 1])
         poison_02_rendered = True
-    poison_02_rect.y += poison_02_speed
+    poison_02_rect.y += poison_02_speed_y
+    poison_02_rect.x += poison_02_speed_x
     display_surface.blit(poison_02_image, poison_02_rect)
 
     if not poison_03_rendered:
@@ -201,9 +214,11 @@ while running:
         poison_03_image = pygame.transform.scale(poison_03_image, (80, 80))
         poison_03_rect = poison_03_image.get_rect()
         poison_03_rect.center = (random.randint(100, WINDOW_WIDTH-100), 110)
-        poison_03_speed = random.randint(2, 8)
+        poison_03_speed_y = random.randint(5, 9)
+        poison_03_speed_x = random.choice([-1, 1])
         poison_03_rendered = True
-    poison_03_rect.y += poison_03_speed
+    poison_03_rect.y += poison_03_speed_y
+    poison_03_rect.x += poison_03_speed_x
     display_surface.blit(poison_03_image, poison_03_rect)
 
     if not poison_04_rendered:
@@ -211,35 +226,100 @@ while running:
         poison_04_image = pygame.transform.scale(poison_04_image, (80, 80))
         poison_04_rect = poison_04_image.get_rect()
         poison_04_rect.center = (random.randint(100, WINDOW_WIDTH-100), 110)
-        poison_04_speed = random.randint(2, 8)
+        poison_04_speed_y = random.randint(5, 9)
+        poison_04_speed_x = random.choice([-1, 1])
         poison_04_rendered = True
-    poison_04_rect.y += poison_04_speed
+    poison_04_rect.y += poison_04_speed_y
+    poison_04_rect.x += poison_04_speed_x
     display_surface.blit(poison_04_image, poison_04_rect)
 
     #Draw platform
-    pygame.draw.rect(display_surface, BROWN, (0, 685, WINDOW_WIDTH, 45))
+    platform_rect = pygame.draw.rect(display_surface, BROWN, (0, 685, WINDOW_WIDTH, 45))
 
     #Move the dragon continuously
     if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and char_rect.left > 0:
         char_rect.x -= VELOCITY
-        if points > 0:
+        if points >= 0:
             display_surface.blit(goodchar_left_image, char_rect)
         else:
             display_surface.blit(evilchar_left_image, char_rect)
     elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and char_rect.right < WINDOW_WIDTH:
         char_rect.x += VELOCITY
-        if points > 0:
+        if points >= 0:
             display_surface.blit(goodchar_right_image, char_rect)
         else:
             display_surface.blit(evilchar_right_image, char_rect)
     else:
-        if points > 0:
+        if points >= 0:
             display_surface.blit(goodchar_stand_image, char_rect)
         else:
             display_surface.blit(evilchar_stand_image, char_rect)
 
+    #Check for collision between two rects
+    prev_points = points
+    if char_rect.colliderect(food_01_rect):
+        food_01_rendered = False
+        points += 1
+        food_sound.play()
+    if char_rect.colliderect(food_02_rect):
+        food_02_rendered = False
+        points += 1
+        food_sound.play()
+    if char_rect.colliderect(food_03_rect):
+        food_03_rendered = False
+        points += 1
+        food_sound.play()
+    if char_rect.colliderect(food_04_rect):
+        food_04_rendered = False
+        points += 1
+        food_sound.play()
+    if char_rect.colliderect(food_05_rect):
+        food_05_rendered = False
+        points += 1
+        food_sound.play()
+    if food_01_rect.colliderect(platform_rect):
+        food_01_rendered = False
+    if food_02_rect.colliderect(platform_rect):
+        food_02_rendered = False
+    if food_03_rect.colliderect(platform_rect):
+        food_03_rendered = False
+    if food_04_rect.colliderect(platform_rect):
+        food_04_rendered = False
+    if food_05_rect.colliderect(platform_rect):
+        food_05_rendered = False
+    if char_rect.colliderect(poison_01_rect):
+        poison_01_rendered = False
+        points -= 1
+        poison_sound.play()
+    if char_rect.colliderect(poison_02_rect):
+        poison_02_rendered = False
+        points -= 1
+        poison_sound.play()
+    if char_rect.colliderect(poison_03_rect):
+        poison_03_rendered = False
+        points -= 1
+        poison_sound.play()
+    if char_rect.colliderect(poison_04_rect):
+        poison_04_rendered = False
+        points -= 1
+        poison_sound.play()
+    if poison_01_rect.colliderect(platform_rect) or poison_01_rect.y > WINDOW_HEIGHT:
+        poison_01_rendered = False
+    if poison_02_rect.colliderect(platform_rect) or poison_02_rect.y > WINDOW_HEIGHT:
+        poison_02_rendered = False
+    if poison_03_rect.colliderect(platform_rect) or poison_03_rect.y > WINDOW_HEIGHT:
+        poison_03_rendered = False
+    if poison_04_rect.colliderect(platform_rect) or poison_04_rect.y > WINDOW_HEIGHT:
+        poison_04_rendered = False
+
+    if prev_points == 0 and points == -1:
+        turn_evil_sound.play()
+    if prev_points == -1 and points == 0:
+        turn_good_sound.play()
+
+
     #Change text color
-    if points > -5:
+    if points >= 0:
         score_text = font_medium.render("Score: " + str(points), True, ORANGE)
         title_text = font_large.render("You Are What You Eat", True, YELLOW)
         timer_text = font_medium.render("time: 99", True, ORANGE)
